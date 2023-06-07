c4da5584bf380131e5bb1585a1cf954da0b717d5
o-brown007 <gboakye97@yahoo.com>
2023-06-07 22:25:51 +0000

    react tests

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+function App(){
+  return (
+    <div>
+      <h1>MY TESTS</h1>
+      <p>This is trials of lessons learnt</p>
+    </div>
+  );
+}
+
+export default App;

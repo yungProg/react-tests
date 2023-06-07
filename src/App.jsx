eddae2c3f84c21fbd0094e9b1ca43f346f063cd6
o-brown007 <gboakye97@yahoo.com>
2023-06-07 22:45:58 +0000

    extracted items from an array into lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import * as React from "react"
 
+const welcome = {
+  title: "Gideon",
+  greeting: "Hey"
+};
+
+const languagesLearnt = ["Python", "HTML", "CSS", "JavaScript"]
+
 function App(){
   return (
     <div>
       <h1>MY TESTS</h1>
       <p>This is trials of lessons learnt</p>
+      <h2>{welcome.greeting} {welcome.title}</h2>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <h2>Programming Languages I have Learnt so Far</h2>
+      <ul>
+        {languagesLearnt.map((lang) => <li>{lang}</li>)}
+      </ul>
     </div>
   );
 }
